e421b8c89d63f850621a51c1e86aa2856233fa4c
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-11-01 19:54:09 -0400

    declared variable in script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const balance = document.getElementById("balance");
+const moneyPlus = document.getElementById("income");
+const moneyMinus = document.getElementById("expense");
+const list = document.getElementById("list");
+const form = document.getElementById("form");
+const text = document.getElementById("text");
+const amount = document.getElementById("amount");
