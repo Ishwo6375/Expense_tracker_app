b45652ed7d779e7e2facfe8193ca9ef7403733a0
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-11-01 19:56:53 -0400

    created dummy transactions

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ const list = document.getElementById("list");
 const form = document.getElementById("form");
 const text = document.getElementById("text");
 const amount = document.getElementById("amount");
+
+const dummyTransactions = [
+    {id:1, text: "Flower", amount: -20},
+    {id:1, text: "Salary", amount: 300},
+    {id:1, text: "Book", amount: -10},
+    {id:1, text: "Camera", amount: 120}
+];
