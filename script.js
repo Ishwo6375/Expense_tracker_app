a23fdb4a4c726cc5802ee1451bc84f5e15223c38
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-11-01 20:07:23 -0400

    implemented function addTransactionDOM

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ const dummyTransactions = [
     {id:1, text: "Book", amount: -10},
     {id:1, text: "Camera", amount: 120}
 ];
+
+function addTransactionDOM(transaction){
+    const sign = transaction.amount > 0 ? "+" : "-";
+    const icon = transaction.amount > 0 ? "up" : "down";
+
+    const item = document.createElement("li");
+    item.classList.add(transaction > 0 ? "plus" : "minus");
+    item.innerHTML = `<h4>${transaction.text}</h4> <span>${sign}$${transaction.amount}<i class="fas fa-caret-${icon}"></i><i class="fas fa-trash-alt"></i></span>`
+}
