59dc1cc6ca4e6acd6aa0ea631e934b050608c4c5
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-11-01 20:12:37 -0400

    created function init

--- script.js
+++ script.js
@@ -10,14 +10,29 @@ const dummyTransactions = [
     {id:1, text: "Flower", amount: -20},
     {id:1, text: "Salary", amount: 300},
     {id:1, text: "Book", amount: -10},
-    {id:1, text: "Camera", amount: 120}
+    {id:1, text: "Camera", amount: -120}
 ];
+
+let transactions = dummyTransactions;
 
 function addTransactionDOM(transaction){
     const sign = transaction.amount > 0 ? "+" : "-";
     const icon = transaction.amount > 0 ? "up" : "down";
 
     const item = document.createElement("li");
-    item.classList.add(transaction > 0 ? "plus" : "minus");
-    item.innerHTML = `<h4>${transaction.text}</h4> <span>${sign}$${transaction.amount}<i class="fas fa-caret-${icon}"></i><i class="fas fa-trash-alt"></i></span>`
+    item.classList.add(transaction.amount > 0 ? "plus" : "minus");
+    item.innerHTML = `<h4>${transaction.text}</h4> <span>${sign}$${Math.abs(
+        transaction.amount)}<i class="fas fa-caret-${icon}"></i><i class="fas fa-trash-alt"></i></span>`
+    list.appendChild(item);
 }
+
+//init
+
+function init(){
+    list.innerHTML = ``
+
+    dummyTransactions.forEach(addTransactionDOM)
+}
+
+init();
+
